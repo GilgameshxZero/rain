347cc674445e0a02becd5fba56434f54c357ee2a
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2020-05-31 21:32:52 -0600

    3.0.1

--- include/rain-version.hpp
+++ include/rain-version.hpp
@@ -1,8 +1,11 @@
 /*
-Automated versioning for rain.
+Version information for `rain`.
 */
+
 #pragma once
+
+#include "rain-version-build.hpp"
+
 #define RAIN_VERSION_MAJOR 3
 #define RAIN_VERSION_MINOR 0
 #define RAIN_VERSION_REVISION 0
-#define RAIN_VERSION_BUILD 13
