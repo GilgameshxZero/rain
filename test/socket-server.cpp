f70d20e29ba8f4b403242ab899c0b77e2ea88948
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2021-01-18 11:02:21 -0700

    6.4.4

--- test/socket-server.cpp
+++ test/socket-server.cpp
@@ -18,10 +18,11 @@ class Server : public Rain::Networking::Server<Rain::Networking::Slave> {
 					Rain::Networking::Socket::RecvFlag::NONE,
 					std::chrono::milliseconds(500));
 				std::cout << "Received " << recvLen
-									<< " bytes: " << std::string(buffer, recvLen) << "\n";
+									<< " bytes: " << std::string(buffer, recvLen) << std::endl;
 			}
 		} catch (const std::exception &e) {
-			std::cout << "Receiving terminated by exception:\n" << e.what() << "\n";
+			std::cout << "Receiving terminated by exception:\n"
+								<< e.what() << std::endl;
 		}
 	}
 };
@@ -30,18 +31,19 @@ int main() {
 	Server server;
 	server.serve(Rain::Networking::Host("localhost", 0), false);
 	Rain::Networking::Host host("localhost", server.getService());
-	std::cout << "Started server on port " << host.service.getCStr() << ".\n";
+	std::cout << "Started server on port " << host.service.getCStr() << "."
+						<< std::endl;
 	Rain::Time::sleepMs(1000);
 	Rain::Networking::Client client;
 	client.connect(host);
-	std::cout << "Connected client.\n";
+	std::cout << "Connected client." << std::endl;
 	Rain::Time::sleepMs(1000);
 	client.send("Hello, world!");
-	std::cout << "Sent: \"Hello, world!\".\n";
+	std::cout << "Sent: \"Hello, world!\"." << std::endl;
 	Rain::Time::sleepMs(1000);
 	client.send("Goodbye!");
-	std::cout << "Sent: \"Goodbye!\".\n";
+	std::cout << "Sent: \"Goodbye!\"." << std::endl;
 	Rain::Time::sleepMs(1000);
-	std::cout << "Stopping server...\n";
+	std::cout << "Stopping server..." << std::endl;
 	return 0;
 }
