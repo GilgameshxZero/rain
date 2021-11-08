58e33e220c9459fd12c8e8fdda97fe36db9aa369
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2021-11-08 12:32:19 -0700

    7.1.15

--- test/data-serializer.cpp
+++ test/data-serializer.cpp
@@ -10,86 +10,26 @@
 int main() {
 	using namespace Rain::Literal;
 
-	{
-		int data[]{1, 4, 5, 10, -5, -49, 3049};
-		int *pData = data + 3;
-		int **pPData = &pData;
-		std::ofstream out("data-serializer.txt", std::ios::binary);
-		Rain::Data::Serializer serializer(out);
-		serializer << data << pData << pPData;
-		out.close();
-		decltype(data) deData;
-		decltype(pData) dePData;
-		decltype(pPData) dePPData;
-		std::ifstream in("data-serializer.txt", std::ios::binary);
-		Rain::Data::Deserializer deserializer(in);
-		deserializer >> deData >> dePData >> dePPData;
-		in.close();
-		assert(memcmp(&data, &deData, sizeof(data)) == 0);
-		assert(memcmp(pData, dePData, sizeof(*pData)) == 0);
-		assert(memcmp(*pPData, *dePPData, sizeof(**pPData)) == 0);
-	}
+	int sData[]{1, 4, 5, 10, -5, -49, 3049};
+	int *sPData = &sData[3];
+	int **sPPData = &sPData;
 
 	{
-		int x = 66;
-		int *pX = &x;
-		std::ofstream out("data-serializer.txt", std::ios::binary);
-		Rain::Data::Serializer serializer(out);
-		serializer << pX;
-		out.close();
-		decltype(pX) dePX = new int;
-		std::ifstream in("data-serializer.txt", std::ios::binary);
-		Rain::Data::Deserializer deserializer(in);
-		deserializer >> dePX;
-		in.close();
-		assert(memcmp(pX, dePX, sizeof(*pX)) == 0);
-		delete dePX;
+		Rain::Data::Serializer serializer("data-serializer.txt");
+		serializer << sData << sPData << sPPData;
 	}
 
-	{
-		int x = 97;
-		int *pX = &x;
-		std::ofstream out("data-serializer.txt", std::ios::binary);
-		Rain::Data::Serializer serializer(out);
-		serializer << x << pX;
-		out.close();
-		decltype(x) deX;
-		decltype(pX) dePX;
-		std::ifstream in("data-serializer.txt", std::ios::binary);
-		Rain::Data::Deserializer deserializer(in);
-		deserializer >> deX >> dePX;
-		in.close();
-		assert(memcmp(&x, &deX, sizeof(x)) == 0);
-		assert(memcmp(pX, dePX, sizeof(*pX)) == 0);
-	}
+	decltype(sData) dData;
+	decltype(sPData) dPData;
+	decltype(sPPData) dPPData;
 
-	{
-		std::vector<long long> a{40586, 2000394953412123LL, 29348, 4059586};
-		std::ofstream out("data-serializer.txt", std::ios::binary);
-		Rain::Data::Serializer serializer(out);
-		serializer << a;
-		out.close();
-		decltype(a) deA;
-		std::ifstream in("data-serializer.txt", std::ios::binary);
-		Rain::Data::Deserializer deserializer(in);
-		deserializer >> deA;
-		in.close();
-		assert(a == deA);
-	}
-
-	{
-		std::string s{"hello world!!! ^_^ :D"};
-		std::ofstream out("data-serializer.txt", std::ios::binary);
-		Rain::Data::Serializer serializer(out);
-		serializer << s;
-		out.close();
-		decltype(s) deS;
-		std::ifstream in("data-serializer.txt", std::ios::binary);
-		Rain::Data::Deserializer deserializer(in);
-		deserializer >> deS;
-		in.close();
-		assert(s == deS);
-	}
+	Rain::Data::Serializer serializer("data-serializer.txt");
+	serializer >> dData >> dPData >> dPPData;
+	assert(memcmp(sData, dData, sizeof(sData)) == 0);
+	assert(dPData == dData + 3);
+	assert(*dPPData == dPData);
+	dData[3] += 93;
+	assert(*dPData == 103);
 
 	return 0;
 }
