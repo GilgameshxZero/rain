ef70adb86d890f4656920cfe55715166b14def8e
GILGAMESH <~@gilgamesh.cc>
2022-01-04 04:51:50 +0000

    7.1.22

---
+++ include/rain/algorithm/mod-ring.hpp
@@ -0,0 +1,132 @@
+// Implementation for a prime modulus ring over the integers, supporting basic
+// operations add, subtract, multiply in O(1) and divide in O(ln N).
+#pragma once
+
+#include "../literal.hpp"
+
+#include <vector>
+
+namespace Rain::Algorithm {
+	// Implementation for a prime modulus ring over the integers, supporting basic
+	// operations add, subtract, multiply in O(1) and divide in O(ln N).
+	//
+	// Integer must be large enough to store (primeModulus - 1)^2.
+	//
+	// For O(1) division, cache multiplicative inverses and multiply with those.
+	template <typename Integer, Integer primeModulus>
+	class ModRing {
+		public:
+		Integer value;
+
+		// Add primeModulus first to wrap back around in the case of "negative"
+		// unsigned Integer.
+		ModRing(Integer value = 0) : value((primeModulus + value) % primeModulus) {}
+
+		// O(ln N) exponentiation.
+		static ModRing<Integer, primeModulus> power(
+			ModRing<Integer, primeModulus> base,
+			Integer exponent) {
+			if (exponent == 0) {
+				return {1};
+			}
+			auto half = power(base, exponent / 2);
+			if (exponent % 2 == 0) {
+				return half * half;
+			} else {
+				return half * half * base;
+			}
+		}
+
+		ModRing<Integer, primeModulus> operator+(Integer other) {
+			return *this + ModRing<Integer, primeModulus>(other);
+		}
+		ModRing<Integer, primeModulus> operator+(
+			ModRing<Integer, primeModulus> other) {
+			return {this->value + other.value};
+		}
+		ModRing<Integer, primeModulus> operator+=(
+			ModRing<Integer, primeModulus> other) {
+			return *this = *this + other;
+		}
+		ModRing<Integer, primeModulus> operator++() { return *this += 1; }
+		ModRing<Integer, primeModulus> operator++(int) {
+			auto tmp(*this);
+			*this += 1;
+			return tmp;
+		}
+		ModRing<Integer, primeModulus> operator-(Integer other) {
+			return *this - ModRing<Integer, primeModulus>(other);
+		}
+		ModRing<Integer, primeModulus> operator-(
+			ModRing<Integer, primeModulus> other) {
+			return {this->value - other.value};
+		}
+		ModRing<Integer, primeModulus> operator-=(
+			ModRing<Integer, primeModulus> other) {
+			return *this = *this - other;
+		}
+		ModRing<Integer, primeModulus> operator--() { return *this -= 1; }
+		ModRing<Integer, primeModulus> operator--(int) {
+			auto tmp(*this);
+			*this -= 1;
+			return tmp;
+		}
+		ModRing<Integer, primeModulus> operator*(Integer other) {
+			return *this * ModRing<Integer, primeModulus>(other);
+		}
+		ModRing<Integer, primeModulus> operator*(
+			ModRing<Integer, primeModulus> other) {
+			return {this->value * other.value};
+		}
+		ModRing<Integer, primeModulus> operator*=(
+			ModRing<Integer, primeModulus> other) {
+			return *this = *this * other;
+		}
+		ModRing<Integer, primeModulus> operator/(Integer other) {
+			return *this / ModRing<Integer, primeModulus>(other);
+		}
+		ModRing<Integer, primeModulus> operator/(
+			ModRing<Integer, primeModulus> other) {
+			return *this * power(other, primeModulus - 2);
+		}
+		ModRing<Integer, primeModulus> operator/=(
+			ModRing<Integer, primeModulus> other) {
+			return *this = *this / other;
+		}
+
+		bool operator==(Integer other) {
+			return *this == ModRing<Integer, primeModulus>(other);
+		}
+		bool operator==(ModRing<Integer, primeModulus> other) {
+			return this->value == other.value;
+		}
+	};
+}
+
+template <typename LeftInteger, typename Integer, Integer primeModulus>
+Rain::Algorithm::ModRing<Integer, primeModulus> operator+(
+	LeftInteger left,
+	Rain::Algorithm::ModRing<Integer, primeModulus> right) {
+	return Rain::Algorithm::ModRing<Integer, primeModulus>(left) + right;
+}
+
+template <typename LeftInteger, typename Integer, Integer primeModulus>
+Rain::Algorithm::ModRing<Integer, primeModulus> operator-(
+	LeftInteger left,
+	Rain::Algorithm::ModRing<Integer, primeModulus> right) {
+	return Rain::Algorithm::ModRing<Integer, primeModulus>(left) - right;
+}
+
+template <typename LeftInteger, typename Integer, Integer primeModulus>
+Rain::Algorithm::ModRing<Integer, primeModulus> operator*(
+	LeftInteger left,
+	Rain::Algorithm::ModRing<Integer, primeModulus> right) {
+	return Rain::Algorithm::ModRing<Integer, primeModulus>(left) * right;
+}
+
+template <typename LeftInteger, typename Integer, Integer primeModulus>
+Rain::Algorithm::ModRing<Integer, primeModulus> operator/(
+	LeftInteger left,
+	Rain::Algorithm::ModRing<Integer, primeModulus> right) {
+	return Rain::Algorithm::ModRing<Integer, primeModulus>(left) / right;
+}
---
+++ test/algorithm-mod-ring.cpp
@@ -0,0 +1,104 @@
+#include <rain/algorithm/mod-ring.hpp>
+
+#include <cassert>
+#include <iostream>
+
+int main() {
+	{
+		Rain::Algorithm::ModRing<long long, 998244353> x, y(100);
+		Rain::Algorithm::ModRing<long long, 1000000009> z;
+		assert(sizeof(long long) >= 8);
+		x += 5;
+		assert(x == 5);
+		x += y;
+		assert(x == 105);
+		x++;
+		assert(x == 106);
+		x -= 6;
+		assert(x == y);
+		x -= 100;
+		assert(x == 0);
+		x -= -35;
+		assert(x == 998244388);
+		x += -50;
+		assert(x == 998244338);
+		// assert(x == z); // Cannot compare between different mods.
+		x = 2;
+		x /= 2;
+		assert(x == 1);
+		x = 343;
+		x /= 7;
+		x *= 14;
+		assert(x == 686);
+		z = 1000000009;
+		assert(z == 0);
+		assert(z == 1000000009);
+		assert(z == 2000000018);
+		z--;
+		assert(z == -1);
+		assert(z == 2000000017);
+		x = 31;
+		assert(y == 100);
+		x *= 2LL * 3 * 5 * 7 * 11 * 13 * 17 * 23 * 29 * 31 * y * 39486758 * y *
+			3049857272LL;
+		y = 2LL * 3 * 5 * 7 * 11 * 13 * 17 * 23 * 29 * 31;
+		assert(x / y / 10000 / 3049857272LL / 31 == 39486758);
+		assert(x / 10000 / 3049857272LL / 39486758 / 31 == y);
+	}
+
+	{
+		Rain::Algorithm::ModRing<std::size_t, 998244353> x, y(100);
+		Rain::Algorithm::ModRing<std::size_t, 1000000009> z;
+		// Make sure this is true!
+		assert(sizeof(std::size_t) >= 8);
+		x += 5;
+		assert(x == 5);
+		x += y;
+		assert(x == 105);
+		x++;
+		assert(x == 106);
+		x -= 6;
+		assert(x == y);
+		x -= 100;
+		assert(x == 0);
+		x -= -35;
+		assert(x == 998244388);
+		x += -50;
+		assert(x == 998244338);
+		// assert(x == z); // Cannot compare between different mods.
+		x = 2;
+		x /= 2;
+		assert(x == 1);
+		x = 343;
+		x /= 7;
+		x *= 14;
+		assert(x == 686);
+		z = 1000000009;
+		assert(z == 0);
+		assert(z == 1000000009);
+		assert(z == 2000000018);
+		z--;
+		assert(z == -1);
+		assert(z == 2000000017);
+		x = 31;
+		assert(y == 100);
+		x *= 2LL * 3 * 5 * 7 * 11 * 13 * 17 * 23 * 29 * 31 * y * 39486758 * y *
+			3049857272LL;
+		y = 2LL * 3 * 5 * 7 * 11 * 13 * 17 * 23 * 29 * 31;
+		assert(x / y / 10000 / 3049857272LL / 31 == 39486758);
+		assert(x / 10000 / 3049857272LL / 39486758 / 31 == y);
+	}
+
+	Rain::Algorithm::ModRing<uint64_t, 998244353> v{99};
+	assert(sizeof(uint64_t) == 8);
+	assert((v + 504957) / 3 == 168352);
+	assert(
+		v - 2038 ==
+		998242414);	 // Must be positive comparison, since std::size_t is positive.
+	assert(v - 2038 == -1939);
+	assert(v - 998244355 == 97);
+
+	auto res = Rain::Algorithm::ModRing<std::size_t, 23>::power(5, 4);
+	assert(res == 625 % 23);
+	return 0;
+}
