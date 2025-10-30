ac2ddd6426a3bc0cf9f913d154994f3aaa919bcb
GILGAMESH <~@gilgamesh.cc>
2025-10-29 21:55:53 -0600

    finish tensor tests

--- include/rain/data.hpp
+++ include/rain/data.hpp
@@ -2,17 +2,3 @@
 #pragma once
 
 #include "data/serializer.hpp"
-
-template <typename Value, std::size_t SIZE>
-inline std::ostream &operator<<(
-	std::ostream &stream,
-	std::array<Value, SIZE> const &values) {
-	if (values.empty()) {
-		return stream << "[]";
-	}
-	stream << '[' << std::setw(4) << values[0];
-	for (std::size_t i{1}; i < SIZE; i++) {
-		stream << ' ' << std::setw(4) << values[i];
-	}
-	return stream << ']';
-}
