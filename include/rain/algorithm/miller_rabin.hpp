6d6b0dcc2013eee710a7f35cbf782ac5dce47211
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-04-27 02:23:33 +0000

    faster miller-rabin with first-class bigint support

--- include/rain/algorithm/miller_rabin.hpp
+++ include/rain/algorithm/miller_rabin.hpp
@@ -1,33 +1,78 @@
 #pragma once
 
 #include "../functional/trait.hpp"
-#include "../random.hpp"
+#include "big_integer.hpp"
 #include "bit_manipulators.hpp"
-#include "modulus_field.hpp"
+
 #include <random>
 
 namespace Rain::Algorithm {
-	// Miller-Rabin helper.
+	// Primality tester for small ints up to 65536, or trivial
+	// composite detector for larger ints.
 	template<
 		typename Integer,
 		std::enable_if<
 			Functional::TraitType<Integer>::IsIntegral::VALUE &&
-			!Functional::TraitType<Integer>::IsSigned::VALUE>::
+			Functional::TraitType<Integer>::IsUnsigned::VALUE>::
 			type * = nullptr>
-	inline bool isMaybePrimeMillerRabinInner(
-		Integer const &N,
-		Integer const &A,
-		std::size_t const lsb,
-		Integer const &truncated) {
-		ModulusField<Integer> X(N);
-		X = X.build(A).power(truncated);
-
-		if (X == 1 || X == -1) {
-			return true;
-		}
-		for (std::size_t j{1}; j < lsb; j++) {
-			X *= X;
-			if (X == -1) {
+	inline bool isTriviallyComposite(Integer N) {
+		static std::uint32_t constexpr PRIMES[]{
+			2,
+			3,
+			5,
+			7,
+			11,
+			13,
+			17,
+			19,
+			23,
+			29,
+			31,
+			37,
+			41,
+			43,
+			47,
+			53,
+			59,
+			61,
+			67,
+			71,
+			73,
+			79,
+			83,
+			89,
+			97,
+			101,
+			103,
+			107,
+			109,
+			113,
+			127,
+			131,
+			137,
+			139,
+			149,
+			151,
+			157,
+			163,
+			167,
+			173,
+			179,
+			181,
+			191,
+			193,
+			197,
+			199,
+			211,
+			223,
+			227,
+			229,
+			233,
+			239,
+			241,
+			251};
+		for (auto &i : PRIMES) {
+			if (N % i == 0) {
 				return true;
 			}
 		}
@@ -35,17 +80,17 @@ namespace Rain::Algorithm {
 	}
 
 	// Miller-Rabin primality test in O(K ln^3 N), with
-	// failure probability upper bounded by 4^-K. Can test
-	// numbers up to half the bits in Integer.
+	// failure probability upper bounded by 4^-K.
 	//
-	// Random generator can only support up to unsigned long
-	// long, so it is not truly random if N is large.
+	// We do not use ModulusField here as modulus can be an
+	// expensive operation on BigInteger, so we perform it
+	// manually.
 	template<
 		typename Integer,
 		typename Generator,
 		std::enable_if<
 			Functional::TraitType<Integer>::IsIntegral::VALUE &&
-			!Functional::TraitType<Integer>::IsSigned::VALUE>::
+			Functional::TraitType<Integer>::IsUnsigned::VALUE>::
 			type * = nullptr,
 		decltype(std::declval<
 			std::uniform_int_distribution<>>()(
@@ -54,64 +99,59 @@ namespace Rain::Algorithm {
 		Integer const &N,
 		std::size_t const K,
 		Generator &generator) {
-		std::uniform_int_distribution<unsigned long long>
-			distribution(
-				2, static_cast<unsigned long long>(N) - 1);
 		if (N < 4) {
 			return N == 2 || N == 3;
 		}
 
-		std::size_t lsb{leastSignificant1BitIdx(N - 1)};
-		Integer truncated{(N - 1) >> lsb};
+		// Larger integer (by 1) needed to store product before
+		// performing modulus.
+		using LargerInteger = BigInteger<
+			MostSignificant1BitIdx<
+				sizeof(Integer) * 8 - 1>::value +
+				2,
+			false>;
+		LargerInteger largerN(N);
+		std::size_t lsb{leastSignificant1BitIdx(largerN - 1)};
+		LargerInteger truncated((largerN - 1) >> lsb);
+
 		for (std::size_t i{0}; i < K; i++) {
-			if (!isMaybePrimeMillerRabinInner(
-						N,
-						static_cast<Integer>(distribution(generator)),
-						lsb,
-						truncated)) {
+			LargerInteger X(1), digit(1), multiple(generator);
+			// Random number in [2, N - 1].
+			multiple = multiple % (largerN - 2) + 2;
+			// Raise X to the power `truncated` under modulo N.
+			for (; digit <= truncated; digit <<= 1) {
+				if ((truncated & digit) != 0) {
+					X = X * multiple % largerN;
+				}
+				multiple = multiple * multiple % largerN;
+			}
+
+			if (X == 1 || X == largerN - 1) {
+				continue;
+			}
+			bool canBePrime{false};
+			for (std::size_t j{1}; j < lsb; j++) {
+				X = X * X % largerN;
+				if (X == largerN - 1) {
+					canBePrime = true;
+					break;
+				}
+			}
+			if (!canBePrime) {
 				return false;
 			}
 		}
 		return true;
 	}
+
+	// Default generator. 64 iterations is generally good for
+	// up to 4096-bit integers.
 	template<typename Integer>
 	inline bool isPrimeMillerRabin(
 		Integer const &N,
-		std::size_t const K) {
+		std::size_t const K = 64) {
 		static std::random_device randomDevice;
-		static std::mt19937_64 generator(randomDevice());
+		static std::mt19937 generator(randomDevice());
 		return isPrimeMillerRabin(N, K, generator);
-	}
-
-	// Miller-Rabin primality test, deterministic, for up to
-	// 64-bits, in O(12). Bound by the same conditions on
-	// Integer as the random version.
-	template<
-		typename Integer,
-		std::enable_if<
-			Functional::TraitType<Integer>::IsIntegral::VALUE &&
-			!Functional::TraitType<Integer>::IsSigned::VALUE>::
-			type * = nullptr>
-	inline bool isPrimeMillerRabinDeterministic(
-		Integer const &N) {
-		if (N < 2) {
-			return false;
-		}
-
-		std::size_t lsb{leastSignificant1BitIdx(N - 1)};
-		Integer truncated{(N - 1) >> lsb};
-		for (
-			auto &a :
-			{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
-			Integer A(a);
-			if (N == A) {
-				return true;
-			}
-			if (!isMaybePrimeMillerRabinInner(
-						N, A, lsb, truncated)) {
-				return false;
-			}
-		}
-		return true;
 	}
 }
--- test/algorithm-miller_rabin.cpp
+++ test/algorithm-miller_rabin.cpp
@@ -1,83 +1,60 @@
 #include <rain.hpp>
+#include <random>
 
 using Rain::Error::releaseAssert;
+using namespace Rain::Algorithm;
+using namespace std;
 
 int main() {
-	using namespace Rain::Literal;
-
-	// Must use LL or _zu literal to avoid overflow during
-	// modding.
+	// BigInt<6>.
 	{
-		auto timeBegin = std::chrono::steady_clock::now();
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabin(
-				1000000009_zu, 100));
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabin(
-				1000000007_zu, 100));
-		releaseAssert(!Rain::Algorithm::isPrimeMillerRabin(
-			1000000005_zu, 100));
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabin(
-				998244353_zu, 100));
-		releaseAssert(!Rain::Algorithm::isPrimeMillerRabin(
-			998244351_zu, 100));
-		auto timeElapsed =
-			std::chrono::steady_clock::now() - timeBegin;
-		std::cout << "Time passed: " << timeElapsed << std::endl;
+		auto timeBegin{std::chrono::steady_clock::now()};
+		releaseAssert(isPrimeMillerRabin(1000000009u));
+		releaseAssert(isPrimeMillerRabin(1000000007u));
+		releaseAssert(!isPrimeMillerRabin(1000000005u));
+		releaseAssert(isPrimeMillerRabin(998244353u));
+		releaseAssert(!isPrimeMillerRabin(998244351u));
+		releaseAssert(isPrimeMillerRabin(29u));
+		releaseAssert(isPrimeMillerRabin(8589934583u));
+		releaseAssert(!isPrimeMillerRabin(8589934581u));
+		auto timeElapsed{
+			std::chrono::steady_clock::now() - timeBegin};
+		cout << "Time: " << timeElapsed << endl;
 	}
 
-	// To test up to 64 bits, need to use 128-bit integer.
+	// BigInt<7>.
 	{
-		auto timeBegin = std::chrono::steady_clock::now();
+		auto timeBegin{std::chrono::steady_clock::now()};
 		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabin(
-				Rain::Algorithm::BigIntegerUnsigned<7>(29), 100));
+			isPrimeMillerRabin(18446744073709551557ULL));
 		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabin(
-				Rain::Algorithm::BigIntegerUnsigned<7>(8589934583),
-				100));
-		releaseAssert(!Rain::Algorithm::isPrimeMillerRabin(
-			Rain::Algorithm::BigIntegerUnsigned<7>(8589934581),
-			100));
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabin(
-				Rain::Algorithm::BigIntegerUnsigned<7>(
-					18446744073709551557ULL),
-				100));
-		releaseAssert(!Rain::Algorithm::isPrimeMillerRabin(
-			Rain::Algorithm::BigIntegerUnsigned<7>(
-				18446744073709551559ULL),
-			100));
-		auto timeElapsed =
-			std::chrono::steady_clock::now() - timeBegin;
-		std::cout << "Time passed: " << timeElapsed << std::endl;
+			!isPrimeMillerRabin(18446744073709551559ULL));
+		auto timeElapsed{
+			std::chrono::steady_clock::now() - timeBegin};
+		cout << "Time: " << timeElapsed << endl;
 	}
 
+	// Generation.
 	{
-		auto timeBegin = std::chrono::steady_clock::now();
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabinDeterministic(
-				1000000009_zu));
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabinDeterministic(
-				1000000007_zu));
-		releaseAssert(
-			!Rain::Algorithm::isPrimeMillerRabinDeterministic(
-				1000000005_zu));
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabinDeterministic(
-				998244353_zu));
-		releaseAssert(
-			!Rain::Algorithm::isPrimeMillerRabinDeterministic(
-				998244351_zu));
-		releaseAssert(
-			Rain::Algorithm::isPrimeMillerRabinDeterministic(
-				Rain::Algorithm::BigIntegerUnsigned<8>(
-					18446744073709551557ULL)));
-		auto timeElapsed =
-			std::chrono::steady_clock::now() - timeBegin;
-		std::cout << "Time passed: " << timeElapsed << std::endl;
+		auto timeBegin{std::chrono::steady_clock::now()};
+		static std::random_device randomDevice;
+		static std::mt19937 generator(randomDevice());
+		BigIntegerUnsigned<7> x;
+		cout << "Finding " << 8 * sizeof(x) << "-bit prime..."
+				 << endl;
+		std::size_t iterations{0};
+		do {
+			x = decltype(x)(generator);
+			iterations++;
+			cout << iterations << ": testing " << x << "..."
+					 << endl;
+		} while (
+			isTriviallyComposite(x) || !isPrimeMillerRabin(x));
+		cout << "Found " << x << " in " << iterations
+				 << " iterations." << endl;
+		auto timeElapsed{
+			std::chrono::steady_clock::now() - timeBegin};
+		cout << "Time: " << timeElapsed << endl;
 	}
 	return 0;
 }
