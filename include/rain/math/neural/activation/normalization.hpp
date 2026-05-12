8106d788a380d128613d172bd3a445188f0943b5
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-05-12 00:56:01 +0000

    clamp normalization, unclamp softmax where possible

--- include/rain/math/neural/activation/normalization.hpp
+++ include/rain/math/neural/activation/normalization.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../../clamp.hpp"
 #include "../activation_interface.hpp"
 
 namespace Rain::Math::Neural::Activation {
@@ -10,19 +11,29 @@ namespace Rain::Math::Neural::Activation {
 		virtual Tensor<Value, 1> apply(
 			Tensor<Value, 1> &z1) const override {
 			// f(x_i) = (x_i - mean(x)) / stddev(x).
-			// Should not need to clamp usually.
-			return z1 = (z1 - z1.mean()) /
-				(z1.standardDeviation() +
-					std::numeric_limits<Value>::min());
+			// Failing to clamp where necessary can block later
+			// Tensor computations and cause high loss!
+			return z1 =
+							 ((z1 - z1.mean()) /
+								 Math::clamp(
+									 z1.standardDeviation() +
+									 std::numeric_limits<Value>::min()))
+								 .clamp();
 		}
 		virtual Tensor<Value, 2> getIncrementalGradient(
 			Tensor<Value, 1> const &z1,
 			Tensor<Value, 1> const &z2) const override {
 			// Compute matrix where g[i][j] = d(f(v_i)) / d(v_j).
-			return (Tensor<Value, 2>::identity(z2.size()[0]) *
-								 z2.size()[0] -
-							 1 - z2.asMultiplyOuter(z2)) /
-				z2.size()[0] / z1.standardDeviation();
+			// Failing to clamp where necessary can block later
+			// Tensor computations and cause high loss!
+			return (
+				(Tensor<Value, 2>::identity(z2.size()[0]) *
+						z2.size()[0] -
+					1 - z2.asMultiplyOuter(z2)) /
+				Math::clamp(
+					z2.size()[0] * z1.standardDeviation() +
+					std::numeric_limits<Value>::min()))
+				.clamp();
 		}
 	};
 }
--- include/rain/math/neural/activation/softmax.hpp
+++ include/rain/math/neural/activation/softmax.hpp
@@ -10,8 +10,13 @@ namespace Rain::Math::Neural::Activation {
 		virtual Tensor<Value, 1> apply(
 			Tensor<Value, 1> &z1) const override {
 			// Easy to overflow during exp!
+			// Range: [+0, +MAX].
 			z1 = z1.exp().clamp();
-			return z1 = (z1 / z1.sum()).clamp();
+			// z1 is finite, z1.sum is [+0, +INF] but is always
+			// at least z1. So, range is finite and needn't be
+			// clamped.
+			return z1 /=
+				z1.sum() + std::numeric_limits<Value>::min();
 		}
 		virtual Tensor<Value, 2> getIncrementalGradient(
 			Tensor<Value, 1> const &,
